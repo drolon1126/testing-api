a91134ced7d6f7a70d3c509578d91c6c38413139
danRolon <daniel.rolon@yahoo.com>
2019-10-03 20:28:05 -0500

    innit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,8 +2,8 @@ const knex = require('knex');
 
 //const secrets = require('./secrets')
 
-const environment = process.env.NODE_ENV || 'development';
-//const environment = secrets.environment || 'development';
-const config = require('../knexfile.js')[environment];
+//const environment = process.env.NODE_ENV || 'development';
+const environment = secrets.environment || 'development';
+const config = require('../knexfile.js')[development];
 
 module.exports = knex(config);
