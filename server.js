5bdee7d3dd4810699fdaf4c27a9af875573191c8
danRolon <daniel.rolon@yahoo.com>
2019-10-03 19:57:15 -0500

    innit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use(logger);
 server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`Let's write some middleware!`)
 });
 
 //custom middleware
